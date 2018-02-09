5f58bc539e598bc5cad9c94fbded826693692c13
Jarno Verhagen <jarnoverhagen@hotmail.com>
2018-02-09 13:27:53 +0100

    Placement of patterns in tree

--- model/pattern/pattern.c
+++ model/pattern/pattern.c
@@ -21,6 +21,89 @@ Pattern *pattern_init(char *name, int width, int height)
     return pattern;
 }
 
-// void tree_place_pattern(QuadTree *tree, Pattern *pattern, int32u_t x, int32u_t y)
-// {
-// }
+void tree_place_pattern(QuadTree *tree, Pattern *pattern, uint64_t x, uint64_t y)
+{
+    SDL_AtomicLock(&tree->write_lock);
+    SDL_AtomicLock(&tree->read_lock);
+
+    uint32_t leaf_min_x = x / 6;
+    uint32_t leaf_min_y = y / 6;
+
+    uint8_t leaf_offset_x = x % 6;
+    uint8_t leaf_offset_y = y % 6;
+
+    printf("x_offset: %d, y_offset: %d\n", leaf_offset_y, leaf_offset_y);
+
+    uint32_t width_leafs = (pattern->width + leaf_offset_x + 5) / 6;
+    uint32_t height_leafs = (pattern->height + leaf_offset_y + 5) / 6;
+
+    for (uint32_t leaf_x = 0; leaf_x < width_leafs; leaf_x++)
+    {
+        for (uint32_t leaf_y = 0; leaf_y < height_leafs; leaf_y++)
+        {
+            // Retrieve leaf that should be edited.
+            Leaf *leaf = tree_get_leaf(tree, leaf_x + leaf_min_x, leaf_y + leaf_min_y);
+            Block data = leaf->data[tree->current_gen];
+
+            if (leaf->x < 1000 || leaf->y < 1000)
+            {
+                printf("a\n");
+            }
+
+            // Loop over internal data of the leaf and edit according to the
+            // pattern.
+            for (int block_x = 1; block_x < 7; block_x++)
+            {
+                for (int block_y = 1; block_y < 7; block_y++)
+                {
+                    uint64_t cell_x = leaf_x * 6 + block_x - 1 - leaf_offset_x;
+                    uint64_t cell_y = leaf_y * 6 + block_y - 1 - leaf_offset_y;
+
+                    // If the cell is outside of the pattern boundaries, it is
+                    // kept unchanged.
+                    if (cell_x < 0 || cell_x >= pattern->width || cell_y < 0 || cell_y >= pattern->height)
+                    {
+                        continue;
+                    }
+
+                    // If the cell is alive in the pattern, it will be alive.
+                    if (pattern->data[cell_x][cell_y])
+                    {
+                        Block mask = 1;
+                        mask <<= (block_x + 8 * block_y);
+                        data |= mask;
+                    }
+                }
+            }
+
+            tree_set_leaf(tree, leaf_x + leaf_min_x, leaf_y + leaf_min_y, data);
+        }
+    }
+
+    SDL_AtomicUnlock(&tree->read_lock);
+    SDL_AtomicUnlock(&tree->write_lock);
+}
+
+void pattern_print(Pattern *pattern)
+{
+    for (int y = 0; y < pattern->height; y++)
+    {
+        for (int x = 0; x < pattern->width; x++)
+        {
+            char c = pattern->data[x][y] ? 'o' : '-';
+            printf("%c", c);
+        }
+        printf("\n");
+    }
+}
+
+void pattern_deinit(Pattern *pattern)
+{
+    for (int i = 0; i < pattern->width; i++)
+    {
+        free(pattern->data[i]);
+    }
+    free(pattern->data);
+
+    free(pattern);
+}
--- model/pattern/pattern.h
+++ model/pattern/pattern.h
@@ -11,3 +11,12 @@ typedef struct Pattern
 
 // Initializes a new pattern instance. Data will be initialized with all death cells.
 Pattern *pattern_init(char *, int, int);
+
+// Adds the given pattern to the tree at the given position.
+void tree_place_pattern(QuadTree *, Pattern *, uint64_t, uint64_t);
+
+// Prints the pattern to console.
+void pattern_print(Pattern *pattern);
+
+// Frees all memory associated with the pattern.
+void pattern_deinit(Pattern *pattern);
